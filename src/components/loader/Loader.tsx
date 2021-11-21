046dc0bd951d88d7853726cef7faae08bb20c246
Passeriform <utbh23041999@gmail.com>
2021-11-22 01:51:59 +0530

    Component: Create loader component.

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,22 @@
+import { Flex, Text, useStyleConfig } from "@chakra-ui/react";
+import Lottie from "react-lottie";
+
+import { LOADER_OPTIONS } from "@config/lottie";
+
+export const Loader = (props: any) => {
+  const { size, text } = props;
+
+  const styles = useStyleConfig("Loader", { size });
+
+  return (
+    <Flex align="center" justify="center" direction="column">
+      <Lottie
+        options={LOADER_OPTIONS}
+        height={styles.height as number}
+        width={styles.width as number}
+        isClickToPauseDisabled
+      />
+      <Text variant="loaderInfo">{text}</Text>
+    </Flex>
+  );
+};
---
+++ src/theme/components/Loader.ts
@@ -0,0 +1,12 @@
+export const Loader = {
+  sizes: {
+    md: {
+      width: 200,
+      height: 200,
+    },
+    lg: {
+      width: 300,
+      height: 300,
+    },
+  },
+};
