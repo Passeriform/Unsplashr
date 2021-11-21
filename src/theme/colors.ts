87b2ef9e8c0bc295b361832fcd1b116efd13dee8
Passeriform <utbh23041999@gmail.com>
2021-11-22 00:49:31 +0530

    Style: Add brand colors.

---
+++ src/theme/colors.ts
@@ -0,0 +1,23 @@
+const colors = {
+  brand: {
+    transparent: "transparent",
+    primary: "#3CB46E",
+    shadow: "rgba(0, 0, 0, 0.05)",
+    50: "#000000",
+    100: "#141414",
+    150: "#232323",
+    200: "#333333",
+    300: "#4F4F4F",
+    350: "#5C5C5C",
+    500: "#858484",
+    550: "#8D8D8D",
+    650: "#A7A7A7",
+    750: "#C4C4C4",
+    800: "#E5E5E5",
+    850: "#ECECEC",
+    875: "#FAFAFA",
+    900: "#FFFFFF",
+  },
+};
+
+export default colors;
