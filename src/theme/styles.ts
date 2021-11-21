6c10b5331a3db5e48fbca7b51633faf0829d6b77
Passeriform <utbh23041999@gmail.com>
2021-11-22 00:45:22 +0530

    Style: Update global style for app.

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,7 +1,16 @@
+import { mode } from "@chakra-ui/theme-tools";
+
 const styles = {
-  baseStyle: {
-    fontFamily: "Montserrat",
-  },
+  global: (props: any) => ({
+    "html, body": {
+      fontFamily: "Montserrat",
+      backgroundColor: mode("brand.900", "brand.150")(props),
+      color: mode("brand.300", "brand.800")(props),
+    },
+    "*, ::before, ::after": {
+      borderColor: mode("brand.850", "brand.500")(props),
+    },
+  }),
 };
 
 export default styles;
